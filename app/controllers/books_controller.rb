5433c0d086778fcf7706565c669674bf65dc2d47
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-05-12 06:19:41 +0000

    save

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new
+    book.save
+    redirect_to '/'
+
   end
 
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+
+    private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
