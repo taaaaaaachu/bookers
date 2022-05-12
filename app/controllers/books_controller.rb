eade6b7565aa22b6a8e94be7af6ad0351ea98ce4
TatsukiYamada <105026743+taaaaaaachu@users.noreply.github.com>
2022-05-12 12:17:57 +0000

    redirect

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,28 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
 
   def create
-    book = Book.new
+    book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to book_path(book.id)
 
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
-    private
-  # ストロングパラメータ
-  def list_params
-    params.require(:list).permit(:title, :body)
+  private
+  def book_params
+   params.require(:book).permit(:title, :body)
   end
 end
